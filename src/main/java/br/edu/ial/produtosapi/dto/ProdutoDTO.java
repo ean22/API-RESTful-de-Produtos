922f4f5def2b8ba36396a9a7e841ac52096edf28
eduardo alves nunes <dualvesnunes@gmail.com>
2026-09-09 15:26:59 -0300

    fead: add Produto DTO

---
+++ src/main/java/br/edu/ial/produtosapi/dto/ProdutoDTO.java
@@ -0,0 +1,13 @@
+package br.edu.ial.produtosapi.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import java.math.BigDecimal;
+
+public record ProdutoDTO(
+        Long id,
+        @NotBlank(message = "O nome e obrigatorio") String nome,
+        String descricao,
+        @Positive(message = "O preco deve ser positivo") BigDecimal preco,
+        Integer estoque
+) {}
