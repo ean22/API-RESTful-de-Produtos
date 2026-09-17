1f01cbf4dc5f6edef780bb7d87b4c01f0c116111
eduardo_alves_nunes <dualvesnunes@gmail.com>
2026-09-17 16:49:05 -0300

    feat: add usuario repository

---
+++ src/main/java/br/edu/ial/produtosapi/repository/UsuarioRepository.java
@@ -0,0 +1,10 @@
+package br.edu.ial.produtosapi.repository;
+
+import br.edu.ial.produtosapi.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    Optional<Usuario> findByNome(String nome);
+}
