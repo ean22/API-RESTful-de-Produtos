d778f1fb1234c4eaff48a7d42dc76c7e5d6b7e19
eduardo_alves_nunes <dualvesnunes@gmail.com>
2026-09-17 21:24:40 -0300

    chore: configure basic auth

--- src/main/java/br/edu/ial/produtosapi/config/SecurityConfig.java
+++ src/main/java/br/edu/ial/produtosapi/config/SecurityConfig.java
@@ -2,6 +2,8 @@ package br.edu.ial.produtosapi.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -10,16 +12,18 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
+                .headers(headers -> headers.frameOptions(frame -> frame.disable()))
                 .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
                 .formLogin(form -> form.disable())
-                .httpBasic(httpBasic -> httpBasic.disable());
-        
+                .httpBasic(Customizer.withDefaults());
+
         return http.build();
     }
 
@@ -28,4 +32,3 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 }
-
